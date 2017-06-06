bb2a1226e93f6dd6fa5f54176868f67cf1b9223e
yuchao <305694029@qq.com>
2017-06-07 00:21:59 +0800

    开发

---
+++ project/admini/en_nav.php
@@ -0,0 +1,11 @@
+<li><a href='./index.php?p=1'>ABOUT US</a></li>
+<li><a href='./index.php?p=2'>PRODUCTS</a><ul><li><a href='./index.php?p=10'>classification1</a></li>
+<li><a href='./index.php?p=11'>classification2</a></li>
+</ul></li>
+<li><a href='./index.php?p=3'>PROJECTS</a></li>
+<li><a href='./index.php?p=4'>NEWS</a></li>
+<li><a href='./index.php?p=5'>CONTACT US</a></li>
+<li><a href='./index.php?p=7'>页面设置</a><ul><li><a href='./index.php?p=6'>Links</a></li>
+<li><a href='./index.php?p=8'>Copyright</a></li>
+<li><a href='./index.php?p=9'>Cpoyright通用</a></li>
+</ul></li>
